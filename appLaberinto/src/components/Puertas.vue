7bbbfca555d1827516b84cf9aa2974e63e84e22f
deivi18contreras <deivibogoya@gmail.com>
2025-11-19 22:00:01 -0500

    appLaberinto

---
+++ appLaberinto/src/components/Puertas.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :style="{ backgroundColor: color }" class="puerta">
+    <p>{{ estado }}</p>
+    <button @click="abrirCerrar">
+      {{ estado === 'abierta' ? 'Cerrar' : 'Abrir' }} 
+    </button>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  estado: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    default: 'brown'
+  }
+})
+
+const emit = defineEmits(['cambiar-estado'])
+
+function abrirCerrar() {
+  const nuevoEstado = props.estado === 'abierta' ? 'cerrada' : 'abierta'
+  emit('cambiar-estado', nuevoEstado)
+}
+</script>
+
+
