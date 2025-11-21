1e67a26c1ad7fbc4cd22a84112a7824d01807294
mikeAbril <jaimecarreno734@gmail.com>
2025-11-20 21:12:22 -0500

    subiendo ultimos trabajos

--- appLaberinto/vite.config.js
+++ appLaberinto/vite.config.js
@@ -3,18 +3,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue({
-          template: { transformAssetUrls }
-  }),
-// @quasar/plugin-vite options list:
-    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+  base: './',   // necesario para deploy en carpeta
+
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
     quasar({
       sassVariables: fileURLToPath(
         new URL('./src/quasar-variables.sass', import.meta.url)
       )
     })
-],
+  ],
+
+  build: {
+    outDir: '../docs/laberinto',   // carpeta donde se generará el build
+  }
 })
